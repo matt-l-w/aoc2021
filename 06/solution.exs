6d9a41b71f0375c103cc5826ec6df0bf8b764d97
Matt Williams <matt@equalcare.coop>
2021-12-06 09:41:26 +0000

    Day 6: Part Two

--- 06/solution.exs
+++ 06/solution.exs
@@ -10,22 +10,30 @@ defmodule Input do
   def test, do: lines('test.txt')
 end
 
-defmodule PartOne do
-  def tick(fish, day\\1)
-  def tick(fish, day) when day < 81 do
-    fish
-    |> Enum.map(&age/1)
-    |> List.flatten
-    |> tick(day+1)
+defmodule PartTwo do
+  def init(input) do
+    for age <- 0..8, do: %{ new: 0, old: Enum.count(input, &(&1 == age)) }
   end
-  def tick(fish, day) when day == 81, do: fish
 
-  def age(fish) when fish == 0, do: [6, 8]
-  def age(fish) when fish > 0, do: fish - 1
+  def tick(counters) do
+    [%{ new: new_dead, old: old_dead } | rest] = counters
+    dead = new_dead + old_dead
+    %{ new: new_sevens, old: old_sevens } = Enum.at(rest, 6)
+    new_counters = List.replace_at(rest, 6, %{ new: new_sevens, old: old_sevens + dead })
+    Enum.concat(new_counters, [%{ new: dead, old: 0 }])
+  end
 end
 
-Input.test
-|> PartOne.tick
-|> Enum.count
-|> IO.inspect
+part_one = PartTwo.init(Input.input)
+1..80
+|> Enum.reduce(part_one, fn _, counters -> PartTwo.tick(counters) end)
+|> Enum.map(fn %{new: new, old: old} -> new + old end)
+|> Enum.sum
+|> IO.inspect(label: "part one")
 
+part_two = PartTwo.init(Input.input)
+1..256
+|> Enum.reduce(part_two, fn _, counters -> PartTwo.tick(counters) end)
+|> Enum.map(fn %{new: new, old: old} -> new + old end)
+|> Enum.sum
+|> IO.inspect(label: "part two")
